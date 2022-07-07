51cc05bffc185adf85cf3a6b5a19f5775650f1d0
callowcreation <callowcreation@gmail.com>
2022-07-07 16:21:53 -0700

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+    return (
+        <Layout>
+        <div>
+            <h1>About</h1>
+            <p>Detail-oriented, organized, and meticulous with over 10 years of experience in software development. Experienced full-stack web developer using React, Angular, Node.js, SQL, and NoSQL databases, desktop applications, and game development.</p>
+            <p>Employee Number 2 at Bitcorn Project. Developed a Twitch chatbot to facilitate the transfer of Bitcorn with chat commands. Developed a Twitch Extension with over 900,000 views and 40,000 unique interactions and over 7 million renders, 4 million of which are unique renders.</p>
+            <p>Unity asset and game developer with 8 assets currently on the Asset Store.</p>
+            <p>Used Microsoft Excel to develop inventory stock tracking spreadsheets for daily placement to different departments on the production line. Promoted to Head Receiver at Skyline within 24 months of employment.</p>
+        </div>
+        </Layout>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/Layout'
+import * as styles from '../styles/home.module.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+    return (
+        <Layout>
+            <section className={styles.header}>
+                <div>
+                    <h2>Software Developer</h2>
+                    <h3>Game Developer</h3>
+                    <p>Full-Stack Web & Unity Game Developer based in California</p>
+                    <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
+                </div>
+                <img src="/banner.png" alt="site banner" style={{maxWidth: '100%'}}/>
+            </section>
+        </Layout>
+    )
 }
