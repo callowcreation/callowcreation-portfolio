1bf350a4d5645d9851f7a10f273e1cef5ba84b4a
callowcreation <callowcreation@gmail.com>
2022-07-08 17:02:14 -0700

    change about page from using html to use a markdown file

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,13 @@ module.exports = {
                 path: `${__dirname}/src/images/`,
             },
         },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/site-information/`,
+            },
+        },
     ],
     siteMetadata: {
         title: 'caLLowCreation',
